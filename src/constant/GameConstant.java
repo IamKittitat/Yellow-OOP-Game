1f06503f6bf79eae8502f222c894f1d479bed016
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-05-13 15:01:59 +0700

    mock gameConstant

--- src/constant/GameConstant.java
+++ src/constant/GameConstant.java
@@ -1,21 +1,29 @@
 package constant;
 
 public class GameConstant {
-//	public static final int PACMAN_SPEED;
-//	public static final int GHOST_SPED;
-//	public static final int BUFF_SPEED;
-//	public static final int DEBUFF_SPEED;
-//	public static final int BUFF_SPAWN_TIME;
-//	public static final int GAME_TIMER;
-//	public static final int PACMAN_LIFE;
-//	public static final int PACMAN_SPAWN_X;
-//	public static final int PACMAN_SPAWN_Y;
-//	public static final int GHOST_SPAWN_X;
-//	public static final int GHOST_SPAWN_Y;
-//	public static final int REVENGE_BUFF_DURATION;
-//	public static final int SPEED_BUFF_DURATION;
-//	public static final int SHIELD_BUFF_DURATION;
-//	public static final int STARVE_BUFF_DURATION;
-//	public static final int SCREEN_SIZE_WIDTH;
-//	public static final int SCREEN_SIZE_HEIGHT;
+	// Not final value of constant
+	public static final int PACMAN_SPEED = 1;
+	public static final int PACMAN_LIFE = 3;
+	public static final int PACMAN_SPAWN_X = 10;
+	public static final int PACMAN_SPAWN_Y = 10;
+	public static final Direction FIRST_PACMAN_DIRECTION = Direction.WEST;
+
+	public static final int GHOST_SPEED = 1;
+	public static final int GHOST_SPAWN_X = 50;
+	public static final int GHOST_SPAWN_Y = 50;
+	public static final Direction FIRST_GHOST_DIRECTION = Direction.NORTH;
+	
+	public static final int BUFF_SPEED = 1;
+	public static final int DEBUFF_SPEED = -1;
+	public static final int BUFF_SPAWN_TIME = 5;
+	public static final int REVENGE_BUFF_DURATION = 8;
+	public static final int SPEED_BUFF_DURATION = 7;
+	public static final int SHIELD_BUFF_DURATION = 7;
+	public static final int STARVE_BUFF_DURATION = 7;
+
+	public static final int GAME_TIMER = 600;
+
+	public static final int SCREEN_SIZE_WIDTH = 900;
+	public static final int SCREEN_SIZE_HEIGHT = 500;
+
 }
