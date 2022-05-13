c446251341e4f4bdc56e7b2112f4c022d0d5c923
ponnabook <ponnapat.book@gmail.com>
2022-05-13 14:18:30 +0700

    Edit MenuControlPane

---
+++ src/application/test.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class test extends Application{
+	 public void start(Stage primaryStage) {
+	  
+	    }
+	    
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	
+}
