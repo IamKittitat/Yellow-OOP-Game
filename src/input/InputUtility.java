bfcbf3947135a00c941fd861d2df693f3d610444
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-05-13 17:35:36 +0700

    input Utility

--- src/input/InputUtility.java
+++ src/input/InputUtility.java
@@ -1,5 +1,28 @@
 package input;
 
-public class InputUtility {
+import javafx.scene.input.KeyCode;
 
+public class InputUtility {
+	private static KeyCode firstPlayerKey;
+	private static KeyCode secondPlayerKey;
+
+	public static boolean getFirstPlayerKeyPressed(KeyCode keycode) {
+		return firstPlayerKey == keycode;
+	}
+
+	public static boolean getSecondPlayerKeyPressed(KeyCode keycode) {
+		return secondPlayerKey == keycode;
+	}
+
+	public static void setKeyPressed(KeyCode keyCode, boolean pressed) {
+		if (pressed) {
+			if ((keyCode == KeyCode.W) || (keyCode == KeyCode.A) || (keyCode == KeyCode.S) || (keyCode == KeyCode.D)) {
+				// if direction same as key dont change
+				firstPlayerKey = keyCode;
+			}
+			if ((keyCode == KeyCode.UP) || (keyCode == KeyCode.RIGHT) || (keyCode == KeyCode.DOWN) || (keyCode == KeyCode.LEFT)) {
+				secondPlayerKey = keyCode;
+			}
+		}
+	}
 }
