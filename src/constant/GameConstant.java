64506ef19e8267f77756a90f4714bbeca99b26e7
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-05-15 11:30:13 +0700

    Total  Pellet constant

--- src/constant/GameConstant.java
+++ src/constant/GameConstant.java
@@ -19,6 +19,7 @@ public class GameConstant {
 	
 	public static final String  PELLET_NAME = "Pellet";
 	public static final String  PELLET_DETAIL = "THE Pellet";
+	public static final int  TOTAL_PELLET = 100;
 	public static final String  REVENGE_BUFF_NAME = "REVENGE_BUFF";
 	public static final String  REVENGE_BUFF_DETAIL = "THE REVENGE_BUFF";
 	public static final String  SPEED_BUFF_NAME = "SPEED_BUFF";
