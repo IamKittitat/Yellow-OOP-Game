469c1c5aa12402d651be9eac255e6762b99f3f06
ponnabook <ponnapat.book@gmail.com>
2022-05-17 11:50:18 +0700

    Update Button

--- src/gui/base/MenuButton.java
+++ src/gui/base/MenuButton.java
@@ -10,54 +10,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import scene.PacManGameScene;
 
-public class MenuButton extends Button {
-//	private Scene bindScene;
-	private GameText buttonText;
+public class MenuButton extends GameButton {
 
-//	public MenuButton(String arg0) {
-//		this(arg0,null);
-//	}
+	private GameText buttonText;
 
 	public MenuButton(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 		this.setPrefWidth(300);
 		this.setPrefHeight(50);
-//		this.bindScene = nextScene;
 		this.setStyle("-fx-background-color: transparent; " + "-fx-border-color: yellow; " + "-fx-border-radius: 30;"+"-fx-text-fill: yellow");
-		setAction();
 	}
 
-	private void setAction() {
-		// TODO Auto-generated method stub
-		setOnMouseEntered(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				DropShadow shadow = new DropShadow();
-				shadow.setColor(Color.YELLOW);
-				setEffect(shadow);
-			}
-		});
 
-		setOnMouseExited(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				setEffect(null);
-			}
-		});
-		
-//		if(this.bindScene != null) {
-//			setOnMouseClicked(new EventHandler<MouseEvent>() {
-//
-//				@Override
-//				public void handle(MouseEvent arg0) {
-//					// TODO Auto-generated method stub
-//					Main.sceneHolder.switchScene(bindScene);
-//				}
-//			});
-//		}
-	}
 
 }
