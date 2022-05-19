6e0983c1a6b73968e45691897c4d2ad61e53f01c
ponnabook <ponnapat.book@gmail.com>
2022-05-19 20:12:21 +0700

    Update BattleGamePane.java

--- src/gui/BattleGamePane.java
+++ src/gui/BattleGamePane.java
@@ -15,8 +15,8 @@ public class BattleGamePane extends StackPane {
 	public BattleGamePane() {
 		super();
 		this.setStyle("-fx-background-color: black;");
-		this.setPrefSize(900, 500);
-		this.setMaxSize(900, 500);
+		this.setPrefSize(912, 500);
+		this.setMaxSize(912, 500);
 
 		this.gameCanvas = new GameCanvas();
 		this.gameControlPane = GameController.gameControlPane;
